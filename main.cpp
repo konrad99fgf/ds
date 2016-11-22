bc5109a544dcedf87a952c93071daf3727500322
konrad99fgf <polska699@interia.eu>
2016-11-22 19:36:32 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <Klub.h>
+#include <Osoba.h>
+#include <Pilkarz.h>
+#include <Rozgrywki.h>
+#include <Gra.h>
+
+
+using namespace std;
+
+int main()
+{
+    Gra moja_grax;
+    moja_grax.wczytaj_gre();
+    return 0;
+}
